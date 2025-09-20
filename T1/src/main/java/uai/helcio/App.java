cef5ac1aaee69c435c86efd530b5c1c243de9ccd
João Gabriel Feres <joaogabrielferes@gmail.com>
2025-09-19 23:13:54 -0300

    Criação da estrutura básica do projeto

---
+++ T1/src/main/java/uai/helcio/App.java
@@ -0,0 +1,8 @@
+package uai.helcio;
+
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ T1/src/test/java/uai/helcio/AppTest.java
@@ -0,0 +1,12 @@
+package uai.helcio;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void dummyTest() {
+        assertEquals(1, Integer.valueOf("1"));
+    }
+}
