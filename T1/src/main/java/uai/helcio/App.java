ac63ae6f10eaa9e5403418cade94b52dd87d255b
João Gabriel Feres <joaogabrielferes@gmail.com>
2025-10-12 15:16:16 -0300

    Utilização de Picoli para CLI
    * Refatorações em ResourcesUtils: Utilização da nova API de gerenciamento de arquivos do java e possibilidade de paralelizar a stream retornada em ResourcesUtils.readFileLines;
    
    * Criação de um arquivo de log, logback.xml, que especifica a estrutura de cada log;
    
    * Adição de flag "parallel" caso o usuário queira que a aplicação seja executada paralelamente;
    
    * Atualização das dependências do projeto.

--- T1/src/main/java/uai/helcio/App.java
+++ T1/src/main/java/uai/helcio/App.java
@@ -1,19 +1,58 @@
 package uai.helcio;
 
 import org.slf4j.event.Level;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 import uai.helcio.converters.ExtendedToPureRegexConverter;
 import uai.helcio.converters.RegexToTreeConverter;
 import uai.helcio.utils.AppLogger;
 import uai.helcio.utils.ResourcesUtils;
 
-import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
-public class App {
-    public static void main(String[] args) {
-        AppLogger.setLoggingLevel(Level.valueOf(args[1]));
-        try (Stream<String> lines = ResourcesUtils.readFileLines(args[0], StandardCharsets.US_ASCII)) {
-            // name: regex
+
+@Command(
+        name = "app",
+        mixinStandardHelpOptions = true,
+        description = "Generates a lexical analyzer from a file containing regular definitions"
+)
+public class App implements Callable<Integer> {
+
+    @Option(
+            names = {"-l", "--log-level"},
+            description = "Specifies the logging level. Possible values: ${COMPLETION-CANDIDATES}. Default: ${DEFAULT-VALUE}.",
+            defaultValue = "TRACE"
+    )
+    private Level logLevel;
+
+    @Option(
+            names = {"-p", "--parallel"},
+            description = "Enables parallel processing."
+    )
+    private boolean parallel;
+
+    @Parameters(
+            index = "0",
+            paramLabel = "INPUT_FILE",
+            description = "Path to the input file containing regular definitions."
+    )
+    private Path inputFile;
+
+    @Override
+    public Integer call() {
+        if (!validateInput()) {
+            return CommandLine.ExitCode.USAGE;
+        }
+
+        AppLogger.setLoggingLevel(logLevel);
+        try (Stream<String> lines = ResourcesUtils.readFileLines(inputFile, parallel)) {
             var a = lines
                     .peek(AppLogger::peekDebug)
                     .map(ExtendedToPureRegexConverter::convert)
@@ -25,5 +64,20 @@ public class App {
         } catch (Exception e) {
             AppLogger.logger.error("Ocorreu um erro durante a geração do analisador léxico", e);
         }
+        return 0;
+    }
+
+    private boolean validateInput() {
+        if (!Files.exists(inputFile)) {
+            AppLogger.logger.error("Arquivo de entrada inexistente");
+            return false;
+        }
+        return Arrays.stream(Level.values())
+                .anyMatch(Predicate.isEqual(logLevel));
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
--- T1/src/main/java/uai/helcio/utils/ResourcesUtils.java
+++ T1/src/main/java/uai/helcio/utils/ResourcesUtils.java
@@ -1,21 +1,19 @@
 package uai.helcio.utils;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.stream.Stream;
 
 public class ResourcesUtils {
-    public static Stream<String> readFileLines(String fileName, Charset charset) throws IOException {
-        BufferedReader fis = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), charset));
-        return fis.lines().onClose(() -> {
-            try {
-                fis.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+    public static Stream<String> readFileLines(Path fileName, boolean parallel) throws IOException {
+        return readFileLines(fileName, StandardCharsets.US_ASCII, parallel);
+    }
+
+    public static Stream<String> readFileLines(Path fileName, Charset charset, boolean parallel) throws IOException {
+        Stream<String> lines = Files.lines(fileName, charset);
+        return parallel ? lines.parallel() : lines;
     }
 }
